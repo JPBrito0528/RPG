d04f7d98142bfe6d60aa317d3ff54d8a0a76e2be
Joao Brito <joao.b@live.com.pt>
2025-04-20 15:20:26 +0100

    changed background

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Prop.h"
 
 int main()
 {
     // initiate window;
-    int windowWidth = 1920;
-    int windowHeight = 1080;
+    int windowWidth = 384;
+    int windowHeight = 384;
     InitWindow(windowWidth, windowHeight, "Neo RPG");
 
     // initiate neo map
-    Texture2D neoMap = LoadTexture("Neo RPG/Neo.png");
+    Texture2D neoMap = LoadTexture("Nature_tileset/natureMap.png");
     Vector2 mapPos{0.0, 0.0};
     const float mapScale = 4.f;
     Character nakedMan{windowWidth, windowHeight};
+
+    Prop rock{Vector2{0.f,0.f}, LoadTexture("nature_tileset/Rock.png")};
+
 
         // initate FPS
         SetTargetFPS(60);
@@ -27,12 +31,15 @@ int main()
         mapPos = Vector2Scale(nakedMan.getWorldPos(), -1.f);
 
         DrawTextureEx(neoMap, mapPos, 0.0, mapScale, WHITE);
+
+        rock.Render(nakedMan.getWorldPos());
+
         nakedMan.tick(GetFrameTime(), windowWidth, windowHeight);
 
-        if (nakedMan.getWorldPos().x < -windowWidth/2 ||
+        if (nakedMan.getWorldPos().x < 0.f||
             nakedMan.getWorldPos().y < 0.f ||
-            nakedMan.getWorldPos().x > neoMap.width * mapScale - windowWidth/2 ||
-            nakedMan.getWorldPos().y > neoMap.height * mapScale - windowHeight/2)
+            nakedMan.getWorldPos().x > neoMap.width * mapScale - windowWidth ||
+            nakedMan.getWorldPos().y > neoMap.height * mapScale - windowHeight)
         {
             nakedMan.undoMovement();
         }
